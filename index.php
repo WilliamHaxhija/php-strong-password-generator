cfa7173b29a09ac5e2e97e0a711eabdaca0e0a40
WilliamHaxhija <williamhaxhija@gmail.com>
2024-05-05 20:48:47 +0200

    feat: add milestone 1

--- index.php
+++ index.php
@@ -1,12 +1,46 @@
-<?php ?>
+<?php
+$charList = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+$passwordLength = intval(isset($_GET['password-length']) ? $_GET['password-length'] : 0);
+
+$userPassword = generateRandPassword($passwordLength, $charList);
+function generateRandPassword($number, $string) {
+    $password = '';
+    $stringLength = strlen($string);
+
+    for ($i = 0; $i < $number; $i++) {
+        $password .= $string[rand(0, $stringLength - 1)];
+    }
+
+    return $password;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-    
+
+<body class="bg-dark">
+    <div class="container d-flex align-items-center justify-content-center flex-column vh-100">
+        <h1 class="text-secondary">Strong Password Generator</h1>
+        <h2 class="text-white">Genera una password sicura</h2>
+        <form class="w-75 h-50 bg-light mt-3 rounded position-relative p-4">
+            <div class="mb-3 d-flex justify-content-between">
+                <label for="password-length" class="form-label">Password Length:</label>
+                <input type="text" class="form-control w-50" id="password-length" name="password-length">
+            </div>
+            <h3><?php echo $userPassword; ?></h3>
+            <button type="submit" class="btn btn-primary position-absolute bottom-0 mb-4">Done</button>
+        </form>
+    </div>
 </body>
+
 </html>
